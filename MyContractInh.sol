ac021f10145ee1a81d8b5dc8832c330ed1334482
Remix Workspace <>
2022-07-25 14:19:09 +0430

    added inheritance

--- MyContractInh.sol
+++ MyContractInh.sol
@@ -1,3 +1,39 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.15;
 
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+}
+
+contract SecretVault {
+    string secret;
+    constructor(string memory _secret) public {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory) {
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
+    address secretVault;
+    constructor(string memory _secret) public {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns(string memory) {
+        return SecretVault(secretVault).getSecret();
+    }
+}
